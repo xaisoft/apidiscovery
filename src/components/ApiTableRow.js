b4db171977a3cc4de4641d6fe934dbf661a55493
saied hassaninia <xaisoft80@gmail.com>
2020-01-04 14:43:03 -0500

    installed styled components

--- src/components/ApiTableRow.js
+++ src/components/ApiTableRow.js
@@ -6,7 +6,7 @@ const ApiTableRow = ({entry}) => {
     <td>{entry.Description}</td>
     <td><a target="_blank" href={entry.Link}>Go to API website</a></td>
     <td>{entry.Auth}</td>
-    <td>{entry.HTTPS ? <FaCheck/>:null}</td>
+    <td style={{textAlign:"center"}}>{entry.HTTPS ? <FaCheck/>:null}</td>
     <td>{entry.Cors}</td>
     <td>{entry.Category}</td>
   </tr>
