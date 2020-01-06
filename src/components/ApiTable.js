6eff23213a7d828cbcdca43b87bb874bb9a07040
saied hassaninia <xaisoft80@gmail.com>
2020-01-05 19:18:35 -0500

    without styled components

--- src/components/ApiTable.js
+++ src/components/ApiTable.js
@@ -1,58 +1,30 @@
 import React from "react";
 import ApiTableRow from "./ApiTableRow";
-import styled from "styled-components";
 
-const Table = styled.table`
-    table-layout: fixed;
-    width:100%;
-    border-collapse: collapse; border: 3px solid purple;
-`
-const TableHeaderCell = styled.th`
-    padding:20px;
-
-    :nth-child(1) {
-        width: 15%;
-
-    :nth-child(2) {
-        width: 30%;
-    }
-
-    :nth-child(3) {
-        width: 30%;
-    }
-}
-`
-
-
-const TableHeader = styled.thead``
-
-
-const TableRow = styled.tr``;
-
-const ApiTable = ({entries}) => {
-    return <Table>
+const Apitable = ({entries}) => {
+    return <table>
     <caption>Public APIs</caption>
-          <TableHeader>
-            <TableRow>
-            <TableHeaderCell>
+          <thead>
+            <tr>
+            <th>
               Title
-            </TableHeaderCell>
-            <TableHeaderCell>
+            </th>
+            <thead>
               Description
-            </TableHeaderCell>
-            <TableHeaderCell>
+            </thead>
+            <th>
               Link
-            </TableHeaderCell>
-            <TableHeaderCell>
+            </th>
+            <th>
               Auth
-            </TableHeaderCell>
-            <TableHeaderCell>
+            </th>
+            <th>
               HTTPS
-            </TableHeaderCell>
-            <TableHeaderCell>Cors</TableHeaderCell>
-            <TableHeaderCell>Category</TableHeaderCell>
-            </TableRow>
-          </TableHeader>
+            </th>
+            <th>Cors</th>
+            <th>Category</th>
+            </tr>
+          </thead>
           <tbody>
           {
    
@@ -61,7 +33,7 @@ const ApiTable = ({entries}) => {
       )
     }
           </tbody>
-        </Table>
+        </table>
 }
 
-export default ApiTable;
+export default Apitable;
--- src/components/ApiTableRow.js
+++ src/components/ApiTableRow.js
@@ -1,27 +1,16 @@
 import React from "react";
 import { FaCheck } from 'react-icons/fa';
-import styled from "styled-components";
 
-const TableRow = styled.tr`
-    :nth-child(even){
-  background-color: #eee;
-}
-`;
-
-const TableCell = styled.td`
-padding: 20px;
-text-align:center;
-`
-const ApiTableRow = ({entry}) => {
-    return <TableRow>
-    <TableCell>{entry.API}</TableCell>
-    <TableCell>{entry.Description}</TableCell>
-    <TableCell><a target="_blank" href={entry.Link}>Go to API website</a></TableCell>
-    <TableCell>{entry.Auth}</TableCell>
-    <TableCell>{entry.HTTPS ? <FaCheck/>:null}</TableCell>
-    <TableCell>{entry.Cors}</TableCell>
-    <TableCell>{entry.Category}</TableCell>
-  </TableRow>
+const Apitr = ({entry}) => {
+    return <tr>
+    <td>{entry.API}</td>
+    <td>{entry.Description}</td>
+    <td><a target="_blank" href={entry.Link}>Go to API website</a></td>
+    <td>{entry.Auth}</td>
+    <td>{entry.HTTPS ? <FaCheck/>:null}</td>
+    <td>{entry.Cors}</td>
+    <td>{entry.Category}</td>
+  </tr>
 }
 
-export default ApiTableRow;
+export default Apitr;
